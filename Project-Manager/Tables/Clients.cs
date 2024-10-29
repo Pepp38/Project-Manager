68981437cd8aa321949f8e5709e77b6f237659ad
Pepp38 <102177070+Pepp38@users.noreply.github.com>
2024-10-29 19:48:34 +0000

    ajustements classe Clients

--- Project-Manager/Tables/Clients.cs
+++ Project-Manager/Tables/Clients.cs
@@ -1,18 +1,15 @@
-// Information sur les clients
-
 using System;
 using System.Collections.Generic;
 
 public class Client
 {
-    public int Id { get; set; }
-    public string Compagnie { get; set; }
-    public string NomContact { get; set; }
-    public string Courriel { get; set; }
-    public string Telephone { get; set; }
-    public string Adresse { get; set; }
-    public string Ville { get; set; }
-    public string Province { get; set; }
-    public string Pays { get; set; }
-    public List<Project> Projects { get; set; }
+    public int Id { get; set; }  // Clé primaire pour le client
+    public string Compagnie { get; set; }  // Nom de la compagnie du client
+    public string NomContact { get; set; }  // Nom de la personne contact
+    public string Ville { get; set; }  // Ville du client
+    public string Province { get; set; }  // Province du client
+    public string Pays { get; set; }  // Pays du client
+
+    // Relation un-à-plusieurs : un client peut avoir plusieurs projets
+    public List<Projet> Projets { get; set; } = new List<Projet>();
 }
